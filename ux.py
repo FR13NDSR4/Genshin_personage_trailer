a6979c847befd953daba8a8be287cb5205ae93da
FR13NDS_R4 <ppr229339@gmail.com>
2023-02-05 16:46:09 +0300

    version1.1(fix)

--- ux.py
+++ ux.py
@@ -1,6 +1,6 @@
 from downloader import *
-import os
 from colorama import *
+import subprocess
 
 init(autoreset=True)
 
@@ -19,19 +19,19 @@ def li_yue():
     if user_choose == 1:
         video = video_downloader("https://youtu.be/_ndVJlqBYLY", "cici")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("cici.mp4")
+        subprocess.call("cici.mp4", shell=True)
     elif user_choose == 2:
         video = video_downloader("https://youtu.be/sjozpa9DsZU", "xiao")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("xiao.mp4")
+        subprocess.call("xiao.mp4", shell=True)
     elif user_choose == 3:
         video = video_downloader("https://youtu.be/qrH9vMZBwAk", "hutao")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("hutao.mp4")
+        subprocess.call("hutao.mp4", shell=True)
     elif user_choose == 4:
         video = video_downloader("https://youtu.be/4oBpaBEMBIM", "zhongli")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("zhongli.mp4")
+        subprocess.call("zhongli.mp4", shell=True)
 
 
 def mondstat():
@@ -48,19 +48,19 @@ def mondstat():
     if user_choose == 1:
         video = video_downloader("https://youtu.be/3VtfVPCbNzk", "fishl")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("fishl.mp4")
+        subprocess.call("fishl.mp4", shell=True)
     elif user_choose == 2:
         video = video_downloader("https://youtu.be/QUioSVENXcI", "mona")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("mona.mp4")
+        subprocess.call("mona.mp4", shell=True)
     elif user_choose == 3:
         video = video_downloader("https://youtu.be/t6BZjmGpq40", "venti")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("venti.mp4")
+        subprocess.call("venti.mp4", shell=True)
     elif user_choose == 4:
         video = video_downloader("https://youtu.be/C_duDk5e8yU", "klee")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("klee.mp4")
+        subprocess.call("klee.mp4", shell=True)
 
 
 def inazuma():
@@ -77,19 +77,19 @@ def inazuma():
     if user_choose == 1:
         video = video_downloader("https://youtu.be/zif0Lmhrivc", "kazuha")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("kazuha.mp4")
+        subprocess.call("kazuha.mp4", shell=True)
     elif user_choose == 2:
         video = video_downloader("https://youtu.be/mvrW4aKwAXw", "raiden")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("raiden.mp4")
+        subprocess.call("raiden.mp4", shell=True)
     elif user_choose == 3:
         video = video_downloader("https://youtu.be/NHEE_-87mNE", "balladeer")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("balladeer.mp4")
+        subprocess.call("balladeer.mp4", shell=True)
     elif user_choose == 4:
         video = video_downloader("https://youtu.be/RPAoXtU5Kgg", "sara")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("sara.mp4")
+        subprocess.call("sara.mp4", shell=True)
 
 
 def sumeru():
@@ -106,16 +106,16 @@ def sumeru():
     if user_choose == 1:
         video = video_downloader("https://youtu.be/oSSCRUmaquo", "tignari")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("tignari.mp4")
+        subprocess.call("tignari.mp4", shell=True)
     elif user_choose == 2:
         video = video_downloader("https://youtu.be/dpZhGzZ3hNc", "nahida")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("nahida.mp4")
+        subprocess.call("nahida.mp4", shell=True)
     elif user_choose == 3:
         video = video_downloader("https://youtu.be/J9WuPBOC_S8", "syno")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("syno.mp4")
+        subprocess.call("syno.mp4", shell=True)
     elif user_choose == 4:
         video = video_downloader("https://youtu.be/P08q5Y7r9Z4", "dori")
         print(f'"{video}" downloaded successfully!!')
-        os.startfile("dori.mp4")
+        subprocess.call("dori.mp4", shell=True)
