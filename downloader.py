87ead62ecfd610fa7196f699feefcf9c2e081a0e
FR13NDS_R4 <87089223+FR13NDSR4@users.noreply.github.com>
2023-02-05 23:18:50 +0300

    1080p

--- downloader.py
+++ downloader.py
@@ -3,5 +3,5 @@ from pytube import YouTube
 
 def video_downloader(video_url, filename):
     my_video = YouTube(video_url)
-    YouTube(video_url).streams.filter(res="720p").first().download(filename=filename + ".mp4")
+    YouTube(video_url).streams.filter(res="1080p").first().download(filename=filename + ".mp4")
     return my_video.title
